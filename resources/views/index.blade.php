5b4bd9b6ebc3565354cd490f17ca5c0c3a7734a4
Gilmar Pereira <gilmarpereira1212@hotmail.com>
2024-10-30 00:41:08 -0300

    [FEAT] LOGO, TAILWIND

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Home</title>
+    
+</head>
+<body class="bg-[#f7ea24]">
+    <img src="{{asset('storage/images/logo.webp')}}" alt="Logo" title="Logo">
+    <h1 class="text-xl">text</h1>
+</body>
+</html>
